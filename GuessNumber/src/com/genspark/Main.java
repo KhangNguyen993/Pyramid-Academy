8dddf6921c162dcbf0e46ee8fb01a0e65c16b389
KhangNguyen993 <khang.nguyen@genspark.net>
2022-02-11 12:04:33 -0600

    Add DragonCave and GuessNumber

---
+++ GuessNumber/src/com/genspark/Main.java
@@ -0,0 +1,75 @@
+package com.genspark;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        boolean playGame = true;
+        int numberGuess = 0;
+        int userInputInteger;
+        int maxGuess = 6;
+
+        int min = 1;
+        int max = 20;
+        int guess = (int)(Math.random()*(max-min+1) + min);  // Guess random number from 1 to 20;
+        String userInput;
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Hello, What is your name?");
+        String userName = scanner.nextLine();
+
+        System.out.println("Well," + userName + ", I am thinking of a number between 1 and 20.");
+
+
+        while (playGame){
+            System.out.println("**************");
+            System.out.println("Take a guess");
+            userInput = scanner.nextLine();
+
+            userInputInteger =Integer.parseInt(userInput);
+
+            if(userInputInteger == guess) {
+                System.out.println("Good job, " + userName + "! You guessed my number in " + numberGuess + " guess(es)!");
+                System.out.println("**************");
+                System.out.println("Would you like to play again?(y or n)");
+
+                String ans = scanner.nextLine();
+                String y; //Yes
+                String n; // No
+                if(ans.equals("y")){
+                    numberGuess =0;
+                    guess = (int)(Math.random()*(max-min+1) + min);
+                } else {
+                    System.out.println("Good bye, "  + userName);
+                    playGame = false;
+                }
+            }
+            else if(userInputInteger > guess){
+                numberGuess ++;
+                System.out.println("Your guess is too high, " + numberGuess + " guess(es)!");}
+            else {
+                numberGuess++;
+                System.out.println("Your guess is too low, " + numberGuess + " guess(es)!");}
+            if(numberGuess == maxGuess ){
+                System.out.println("You Lose! Good Luck next time!");
+                System.out.println("Would you like to play again?(y or n)");
+
+                String ans = scanner.nextLine();
+                String y; //Yes
+                String n; // No
+                if(ans.equals("y")){
+                    numberGuess =0;
+                    guess = (int)(Math.random()*(max-min+1) + min);
+                } else {
+                    System.out.println("Good bye, "  + userName + "!");
+                    playGame = false;
+                }
+            }
+
+        }
+
+    }
+}
